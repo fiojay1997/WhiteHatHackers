657d20b8df4b4b3ab28525c842ee017ae1078052
fiojay1997 <fio827601499@gmail.com>
2020-11-09 19:58:12 -0700

    update apii caller

--- python/api_call.py
+++ python/api_call.py
@@ -9,7 +9,7 @@ import re
 def make_header(browser_info, arguments, inputs):
     header = {}
     if browser_info is not None:
-        header['User-Agent'] = browser_info.strip().lower()
+        header['User-Agent'] = browser_info.lstrip()
     else:
         # use firefox as default
         header['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'
