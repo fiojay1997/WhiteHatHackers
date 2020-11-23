aea1f1c80c0a4c9ce3e4677c55f2d4d8cc886c57
Kaijie Fu <kaijiefu@Kaijies-MacBook-Pro.local>
2020-11-22 19:24:29 -0700

    resturcture

--- python/api_script.py
+++ python/api_script.py
@@ -4,7 +4,70 @@ import time
 import os
 import re
 from pathlib import Path
-import python.api_db as db
+import datetime
+import pymysql
+
+
+def read_db_config(dest="db_config.json"):
+    f = open(dest)
+    return json.load(f)
+
+
+def db_con(config):
+    if 'host' not in config \
+            or 'user' not in config \
+            or 'password' not in config\
+            or 'db' not in config:
+        raise ValueError
+
+    host = config['host']
+    user = config['user']
+    password = config['password']
+    db = config['db']
+    port = None
+    if 'port' in config:
+        port = config['port']
+
+    if port is not None:
+        try:
+            connection = pymysql.connect(host=host, user=user, password=password, db=db, port=port)
+            return connection
+        except AssertionError:
+            return
+    else:
+        try:
+            connection = pymysql.connect(host=host, user=user, password=password, db=db, port=3306)
+            return connection
+        except AssertionError:
+            return
+
+
+def insert_data(data_map):
+    config = read_db_config()
+    table = config["table"]
+    connection = db_con(config)
+
+    try:
+        with connection.cursor() as cursor:
+            if 'date' not in data_map:
+                data_map['date'] = datetime.datetime.now()
+            keys = list(data_map.keys())
+            values = list(data_map.values())
+            key_str = ''
+            value_str = ''
+            for k in range(len(keys) - 1):
+                key_str += str(keys[k]) + ", "
+            key_str += str(keys[-1])
+            for v in range(len(values) - 1):
+                value_str += "'" + str(values[v]) + "'" + ", "
+            value_str += "'" + str(values[-1]) + "'"
+
+            sql = "INSERT INTO " + table + " (" + key_str + ")" + " VALUES " + "(" + value_str + ")"
+            print(sql)
+            cursor.execute(sql)
+        connection.commit()
+    finally:
+        connection.close()
 
 
 # make header for the request
@@ -254,7 +317,7 @@ def proceed(keywords=None, file_dest="data",
             else:
                 nested_info[key] = return_info[key]
         print(nested_info)
-        db.insert_data(nested_info)
+        insert_data(nested_info)
     else:
         save_json(response)
 
