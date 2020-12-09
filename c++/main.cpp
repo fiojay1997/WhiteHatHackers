5f58c6cbd339ef979722375ac75a2ff4174246ae
Jacob Jenn <jakejenn21@gmail.com>
2020-12-08 20:18:32 -0700

    added logging / inline documentation

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,22 +1,34 @@
+/**
+ * TEAM: WHITEHATHACKERS 
+ * MEMBERS: Jake, Leon, Nanda, Kaijie, Sephora, Austin
+ * DATE: 10/08/2020
+ * 
+ **/
+
 #include "video.cpp"
 #include <vector>
 
 using namespace std;
 
-int main()
+/**
+ * Main starting point of program
+ * Final Video product svg_image.mp4
+ **/
+int main(int argc, char *argv[])
 {
+    //set variables for final movie output
     const int width = 1080;
     const int height = 900;
     const int nframes = 100;
 
+    //initialize video
     video movie("svg_image", width, height);
 
+    //for each frame of movie
     for (int iframe = 0; iframe < nframes; iframe++)
     {
-        
-        movie.addFrame("test.png");
-        movie.addFrame("output.png");
-        movie.addFrame("sun.png");
+        //add frame
+        movie.addFrame(argv[1]);
     }
     
 }
